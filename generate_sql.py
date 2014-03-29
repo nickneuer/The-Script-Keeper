f8e66c24cc92094f3a1e4af80353529564a57e5d
Nick Neuer <nick.a.neuer@gmail.com>
2014-03-29 11:15:06 -0700

    added "if not exists" statement

--- generate_sql.py
+++ generate_sql.py
@@ -1,14 +1,12 @@
 import sys 
 
 SQL = """
-DROP TABLE books;
-
-CREATE TABLE books(author varchar, title varchar, ISBN int, binding varchar, price float, BISAC varchar, publisher varchar, date_pub varchar, TW int, YTD int, RTD int);
+CREATE TABLE IF NOT EXISTS {1}(author varchar, title varchar, ISBN int, binding varchar, price float, BISAC varchar, publisher varchar, date_pub varchar, TW float, YTD float, RTD float);
 
 .mode csv
 .separator ';'
-.import {0} books
+.import {0} {1}
 """
 
 with open("books_populate.sql", "wb") as out:
-    out.write(SQL.format(sys.argv[1]))
+    out.write(SQL.format(sys.argv[1], sys.argv[2]))
